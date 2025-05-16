3f269dcfb53bfbafae8355f293ebfa8827611201
easyaitutor <toyzinternational@gmail.com>
2025-05-16 11:20:46 -0500

    Update ai_tutor_full.py

--- ai_tutor_full.py
+++ ai_tutor_full.py
@@ -778,7 +778,6 @@ def build_instructor_ui():
                     outputs=[contact_status_output, contact_name, contact_email_addr, contact_message, contact_attachment],
                     queue=True
                     )
-                instructor_demo.queue()
                 return instructor_demo  # Return the Blocks instance
 
  
